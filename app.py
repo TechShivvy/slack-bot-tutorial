c3e7d95a8ef6a811bc0b1eca1f1493d34c20dccd
Shivcharan Thirunavukkarasu <132028666+TechShivvy@users.noreply.github.com>
2024-01-31 02:12:23 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+env_path = Path(".") / ".env"
+
+load_dotenv(dotenv_path=env_path)
+
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"), name="BroKOLee Bot")
+
+
+@app.event("app_home_opened")
+def say_hw(message, say):
+    say(f"hello world ")
+
+
+@app.event("message")
+def handle_message(ack, body, say):
+    user_id = body["event"]["user"]
+    channel_id = body["event"]["channel"]
+    text = body["event"]["text"]
+
+    ack()
+
+    if "hello" in text.lower():
+        say(f"Hi <@{user_id}>! How can I help you?")
+
+
+def main():
+    handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
+    handler.start()
+
+
+if __name__ == "__main__":
+    main()
